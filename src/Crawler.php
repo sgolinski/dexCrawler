df4b4017ed679ea6e88f00134c51799d9ef3401d
sgolinski <simon.golinski@gmail.com>
2022-07-17 15:03:33 +0200

    change cron value

--- src/Crawler.php
+++ src/Crawler.php
@@ -47,7 +47,7 @@ EOF;
             sleep(1);
 
             for ($i = 0; $i < 10; $i++) {
-                $this->client->takeScreenshot('page' . $i . '.jpg');
+                //  $this->client->takeScreenshot('page' . $i . '.jpg');
                 $this->client->refreshCrawler();
                 $data = $this->getContent();
                 $this->getBnbOrUsd($data);
@@ -91,7 +91,7 @@ EOF;
         foreach ($content as $webElement) {
 
             assert($webElement instanceof RemoteWebElement);
-            echo self::$counter++;
+            // echo self::$counter++;
             $information = $webElement->findElement(WebDriverBy::cssSelector('tr > td:nth-child(5)'))->getText();
             if ($information === null) {
                 continue;
