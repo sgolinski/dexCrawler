1c05e0cde6c504f1a0f25d9e482fa2c378f166c8
sgolinski <simon.golinski@gmail.com>
2022-07-24 18:07:09 +0200

    change host

--- src/Writer/FileWriter.php
+++ src/Writer/FileWriter.php
@@ -7,7 +7,7 @@ class FileWriter implements Writer
 
     public static function write(array $makers): void
     {
-        file_put_contents('tokens_already_recorded.txt', serialize($makers));
+        file_put_contents('/mnt/app/tokens_already_recorded.txt', serialize($makers));
     }
 
     public static function writeOne(string $alert)
